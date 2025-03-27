a912a1c94a88753c2988387e5742bb709776bd01
Mijanur Rahman <mijan.cse19@gmail.com>
2025-03-27 22:00:04 +0600

    profile section updated

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -84,3 +84,30 @@ export const logout = async (_, res) => {
         });
     }
 };
+
+// User profile================
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const userId = req.id;
+        const user = await User.findById(userId)
+            .select('-password')
+            .populate('enrolledCourses');
+        if (!user) {
+            return res.status(404).json({
+                message: 'Profile not found',
+                success: false
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Failed to load user'
+        });
+    }
+};
