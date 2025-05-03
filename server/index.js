7cca7b9583a80c1a9d2c3e027393e6408ed4383a
Mijan2001 <mijan.cse19@gmail.com>
2025-05-04 02:20:11 +0600

    updated index.js file

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
 // app.use(
