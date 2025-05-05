1b94dfb34312d5a6f5584bd8a14b48a3cf35f718
Mijan2001 <mijan.cse19@gmail.com>
2025-05-05 15:42:50 +0600

    updated

--- server/index.js
+++ server/index.js
@@ -18,13 +18,13 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-// app.use(cors());
-app.use(
-    cors({
-        origin: [process.env.CLIENT_URL, 'http://localhost:5173/'],
-        credentials: true
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: [process.env.CLIENT_URL, 'http://localhost:5173/'],
+//         credentials: true
+//     })
+// );
 
 const PORT = process.env.PORT || 3000;
 
