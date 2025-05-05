79aeb7a1c4ce1603616b02e7640b559c063ef844
Mijan2001 <mijan.cse19@gmail.com>
2025-05-05 18:08:11 +0600

    fixing cors error

--- server/index.js
+++ server/index.js
@@ -18,13 +18,14 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors());
-// app.use(
-//     cors({
-//         origin: [process.env.CLIENT_URL, 'http://localhost:5173/'],
-//         credentials: true
-//     })
-// );
+app.use(
+    cors({
+        origin: [process.env.CLIENT_URL], // or hardcode: 'https://lms-mern-stack.vercel.app'
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    })
+);
 
 const PORT = process.env.PORT || 3000;
 
