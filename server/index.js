53b147956fe73eea58189d6c6b4073ea947a4c00
Mijan2001 <mijan.cse19@gmail.com>
2025-05-13 17:03:42 +0600

    updated index.js file

--- server/index.js
+++ server/index.js
@@ -21,10 +21,7 @@ app.use(morgan('dev'));
 app.use(
     cors({
         origin: '*',
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-        exposedHeaders: ['Set-Cookie']
+        credentials: true
     })
 );
 
